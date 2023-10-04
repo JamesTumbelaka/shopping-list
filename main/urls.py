98ec7c090c6cd580266772820283f0edf2360812
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-10-04 15:12:17 +0700

    tutorial 5 selesai

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from main.views import show_main, create_product, edit_product, delete_product
 from main.views import show_xml, show_json
 from main.views import  show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user
+from main.views import get_product_json
+from main.views import add_product_ajax
 
 app_name = 'main'
 
@@ -18,4 +20,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
 ]
