0e9dddf505fe0aaf6e9137a597bc85e6338f8e55
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-11-15 13:18:22 +0700

    redeploy 4

--- main/views.py
+++ main/views.py
@@ -12,6 +12,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseNotFound
+from django.http import JsonResponse
+import json
 import datetime
 
 
@@ -129,3 +131,22 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
